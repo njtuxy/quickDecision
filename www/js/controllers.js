8d239f0e510478c2c5e904fb6f1816e890532183
Yan Xia <yxia@kabaminc.com>
2015-08-06 15:55:24 -0700

    working check boxes

---
+++ www/js/controllers.js
@@ -0,0 +1,10 @@
+/**
+ * Created by yxia on 8/6/15.
+ */
+
+//angular.module('starter.controllers', [])
+//    .controller('gestureController', function($scope){
+//        $scope.onDragUp = function(){
+//            $scope.debugText = "new Text";
+//        }
+//    });
