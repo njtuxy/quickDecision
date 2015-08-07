d4a7d1f40a8e9074cc1c40885b3671151ede352c
Yan Xia <yxia@kabaminc.com>
2015-08-06 17:35:44 -0700

    beautiful card content layout

--- www/js/app.js
+++ www/js/app.js
@@ -49,7 +49,7 @@ angular.module('app', ['ionic'])
 
         // if none of the above states are matched, use this as the fallback
 
-        $urlRouterProvider.otherwise('/login');
+        $urlRouterProvider.otherwise('/contents');
     })
     .controller('gestureController', function($scope){
         //$scope.onDragLeft = function(){
