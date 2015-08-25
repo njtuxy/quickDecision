65ed4d5e504c2cbc32d0b5ff91d2614f585923a5
Yan Xia <njtuxy@hotmail.com>
2015-08-24 21:35:21 -0700

    working side-menu and tabs

--- www/js/app.js
+++ www/js/app.js
@@ -26,138 +26,192 @@ angular.module('qd', ['ionic', 'qd.controllers', 'qd.services', 'ngCordova', 'fi
         // Set up the various states which the app can be in.
         // Each state's controller can be found in controllers.js
 
-
         $stateProvider
-            .state("auth", {
-                url: "/auth",
-                templateUrl: "templates/login/auth.html",
-                abstract: true
-
+            .state("app", {
+                url: "/app",
+                abstract: true,
+                templateUrl: "templates/theme1/side-menu.html"
             })
 
-            .state("auth.welcome", {
-                url: "/welcome",
-                templateUrl: "templates/login/welcome.html",
-                controller: "WelcomeCtrl"
-            })
-
-            .state("shop", {
+            .state("app.shop", {
                 url: "/shop",
                 abstract: true,
                 views: {
-                    "menuContent": {templateUrl: "templates/shop/shop.html"}
-                }
-            })
-
-            .state("shophome", {
-                url: "/shophome",
-                templateUrl: "templates/shop/shop-home.html",
-                //templateUrl: 'templates/contents.html'
-                controller: "ShopController"
-            })
-
-
-            //.state("app.shop.home", {
-            //    url: "/home",
-            //    views: {
-            //        "shop-home": {
-            //            templateUrl: "templates/shop/shop-home.html",
-            //            controller: "ShopController"
-            //        }
-            //    }
-            //})
-
-            //.state('loginPage', {
-            //    url: '/login',
-            //    templateUrl: 'templates/login.html',
-            //    controller: 'loginController'
-            //})
-            //
-            //.state('sign-up', {
-            //    url: '/sign-up',
-            //    templateUrl: 'templates/sign-up.html',
-            //    controller: 'singUpController'
-            //})
-
-            .state('imageUpload', {
-                url: '/imageUpload',
-                templateUrl: 'templates/imageUpload.html',
-                controller: 'imageUploadController'
-            })
-
-            //.state('landingPage', {
-            //    url: '/landing',
-            //    templateUrl: 'templates/landing.html'
-            //})
-
-
-            .state('contents', {
-                url: '/contents',
-                templateUrl: 'templates/contents.html'
-            })
-
-            //Cannot debug when using Controller.
-            .state('camera', {
-                url: '/camera',
-                templateUrl: 'templates/camera.html',
-                //controller: 'CameraController'
-            })
-
-            .state('imageDetail', {
-                url: '/imageDetail',
-                templateUrl: 'templates/imageDetail.html'
-            })
-
-            // setup an abstract state for the tabs directive
-            .state('tab', {
-                url: '/tab',
-                abstract: true,
-                templateUrl: 'templates/tabs.html'
-            })
-
-            // Each tab has its own nav history stack:
-
-            .state('tab.dash', {
-                url: '/dash',
-                views: {
-                    'tab-dash': {
-                        templateUrl: 'templates/tab-dash.html',
-                        controller: 'DashCtrl'
+                    "menuContent": {
+                        templateUrl: "templates/shop/shop.html"
                     }
                 }
             })
 
-
-            .state('tab.chats', {
-                url: '/chats',
+            .state('app.shop.home', {
+                url: '/',
                 views: {
-                    'tab-chats': {
-                        templateUrl: 'templates/tab-chats.html',
-                        controller: 'ChatsCtrl'
-                    }
-                }
-            })
-            .state('tab.chat-detail', {
-                url: '/chats/:chatId',
-                views: {
-                    'tab-chats': {
-                        templateUrl: 'templates/chat-detail.html',
-                        controller: 'ChatDetailCtrl'
+                    'shop-home': {
+                        templateUrl: 'templates/shop/shop-home.html'
+                        //controller: "ShopController"
                     }
                 }
             })
 
-            .state('tab.account', {
-                url: '/account',
+            .state('app.shop.popular', {
+                url: '/shop-popular',
                 views: {
-                    'tab-account': {
-                        templateUrl: 'templates/contents.html',
-                        controller: 'AccountCtrl'
+                    'shop-popular': {
+                        templateUrl: 'templates/test/view2.html'
+                    }
+                }
+            })
+
+            .state('app.shop.sale', {
+                url: '/shop-sale',
+                views: {
+                    'shop-sale': {
+                        templateUrl: 'templates/test/view3.html'
                     }
                 }
             });
 
-        // if none of the above states are matched, use this as the fallback
+        //.state('app.contents', {
+        //    url: '/contents',
+        //    views: {
+        //        'menuContent': {
+        //            templateUrl: 'templates/contents.html'
+        //        }
+        //    }
+        //})
 
-        $urlRouterProvider.otherwise('/shophome');
-    });
+        //.state("auth", {
+        //    url: "/auth",
+        //    templateUrl: "templates/login/auth.html",
+        //    abstract: true
+        //
+        //})
+        //
+        //.state("auth.welcome", {
+        //    url: "/welcome",
+        //    templateUrl: "templates/login/welcome.html",
+        //    controller: "WelcomeCtrl"
+        //})
+        //
+        //
+        //.state("shophome", {
+        //    url: "/shop-home",
+        //    templateUrl: "templates/shop/shop-home.html",
+        //    //templateUrl: 'templates/contents.html'
+        //    controller: "ShopController"
+        //})
+        //
+        //
+        ////.state("app.shop.home", {
+        ////    url: "/home",
+        ////    views: {
+        ////        "shop-home": {
+        ////            templateUrl: "templates/shop/shop-home.html",
+        ////            controller: "ShopController"
+        ////        }
+        ////    }
+        ////})
+        //
+        ////.state('loginPage', {
+        ////    url: '/login',
+        ////    templateUrl: 'templates/login.html',
+        ////    controller: 'loginController'
+        ////})
+        ////
+        ////.state('sign-up', {
+        ////    url: '/sign-up',
+        ////    templateUrl: 'templates/sign-up.html',
+        ////    controller: 'singUpController'
+        ////})
+        //
+        //.state('imageUpload', {
+        //    url: '/imageUpload',
+        //    templateUrl: 'templates/imageUpload.html',
+        //    controller: 'imageUploadController'
+        //})
+        //
+        ////.state('landingPage', {
+        ////    url: '/landing',
+        ////    templateUrl: 'templates/landing.html'
+        ////})
+        //
+        //
+        ////Cannot debug when using Controller.
+        //.state('camera', {
+        //    url: '/camera',
+        //    templateUrl: 'templates/camera.html',
+        //    //controller: 'CameraController'
+        //})
+        //
+        //.state('imageDetail', {
+        //    url: '/imageDetail',
+        //    templateUrl: 'templates/imageDetail.html'
+        //})
+        //
+
+        //setup an abstract state for the tabs directive
+        //
+        //.state('tab', {
+        //    url: '/tab',
+        //    abstract: true,
+        //    templateUrl: 'templates/tabs.html'
+        //})
+        //
+        //
+        //// Each tab has its own nav history stack:
+        //
+        //.state('tab.dash', {
+        //    url: '/dash',
+        //    views: {
+        //        'tab-dash': {
+        //            templateUrl: 'templates/tab-dash.html',
+        //            controller: 'DashCtrl'
+        //        }
+        //    }
+        //})
+        //
+        //
+        //.state('tab.chats', {
+        //    url: '/chats',
+        //    views: {
+        //        'tab-chats': {
+        //            templateUrl: 'templates/tab-chats.html',
+        //            controller: 'ChatsCtrl'
+        //        }
+        //    }
+        //})
+        //.state('tab.chat-detail', {
+        //    url: '/chats/:chatId',
+        //    views: {
+        //        'tab-chats': {
+        //            templateUrl: 'templates/chat-detail.html',
+        //            controller: 'ChatDetailCtrl'
+        //        }
+        //    }
+        //})
+        //
+        //.state('tab.account', {
+        //    url: '/account',
+        //    views: {
+        //        'tab-account': {
+        //            templateUrl: 'templates/contents.html',
+        //            controller: 'AccountCtrl'
+        //        }
+        //    }
+        //})
+        //
+        //.state('contents', {
+        //    url: '/contents',
+        //    views: {
+        //        'menuContent': {
+        //            templateUrl: 'templates/contents.html'
+        //        }
+        //    }
+        //});
+
+// if none of the above states are matched, use this as the fallback
+
+        $urlRouterProvider.otherwise('/app/shop/');
+        //$urlRouterProvider.otherwise('/shop-home');
+    })
+;
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -281,7 +281,18 @@ angular.module('qd.controllers', ['firebase'])
         $http.get("testdata.json").success(function (data) {
             $scope.products = data.products;
         });
-    });
+    })
+
+    .controller('PlaylistsCtrl', function ($scope) {
+        $scope.playlists = [
+            {title: 'Reggae', id: 1},
+            {title: 'Chill', id: 2},
+            {title: 'Dubstep', id: 3},
+            {title: 'Indie', id: 4},
+            {title: 'Rap', id: 5},
+            {title: 'Cowbell', id: 6}
+        ];
+    })
 
 
 
